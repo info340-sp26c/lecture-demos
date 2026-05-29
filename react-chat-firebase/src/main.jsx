db2cd108fb2a7732145f783dd30c89ab247cf5a0
Tim Carlson <timca@arenasports.net>
2026-05-28 17:39:05 -0700

    end of firebase db lecture

--- react-chat-firebase/src/main.jsx
+++ react-chat-firebase/src/main.jsx
@@ -9,24 +9,26 @@ import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
  
-// //Slide 16
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+//Slide 16
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAuIRlkrH1YQ9T3YLcarj70OD8N4F6aTgk",
-//   authDomain: "foo-delete-a4f73.firebaseapp.com",
-//   projectId: "foo-delete-a4f73",
-//   storageBucket: "foo-delete-a4f73.firebasestorage.app",
-//   messagingSenderId: "716284348433",
-//   appId: "1:716284348433:web:3259d592c76b019f5c89b0"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFE67TDob8M6k68knSQ2HpjtjdwVlfTZE",
+  authDomain: "chat3-info340c-sp26.firebaseapp.com",
+  databaseURL: "https://chat3-info340c-sp26-default-rtdb.firebaseio.com",
+  projectId: "chat3-info340c-sp26",
+  storageBucket: "chat3-info340c-sp26.firebasestorage.app",
+  messagingSenderId: "712952001170",
+  appId: "1:712952001170:web:4f55a845de3f1128ddec4b"
+};
 
-// // Initialize Firebase
-// initializeApp(firebaseConfig);
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
